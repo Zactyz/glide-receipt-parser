10f3d11ac38b4a6799b44e4af776c4ae1b155178
Zactyz18 <zzimmerman@iswhub.com>
2025-11-19 15:15:39 -0500

    Add console logging to driver.js to debug why function isn't being called

--- driver.js
+++ driver.js
@@ -1,10 +1,14 @@
 window.addEventListener("message", async function(event) {
+  console.log('[Receipt Parser Driver] Message received:', event.data);
   const { origin, data: { key, params } } = event;
   let result;
   let error;
   try {
+    console.log('[Receipt Parser Driver] Calling function with params:', params);
     result = await window.function(...params);
+    console.log('[Receipt Parser Driver] Function returned:', result);
   } catch (e) {
+    console.error('[Receipt Parser Driver] Function error:', e);
     result = undefined;
     try {
       error = e.toString();
@@ -19,6 +23,7 @@ window.addEventListener("message", async function(event) {
   if (error !== undefined) {
     response.error = error;
   }
+  console.log('[Receipt Parser Driver] Sending response:', response);
   event.source.postMessage(response, "*");
 });
 
